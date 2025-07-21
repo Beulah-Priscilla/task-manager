bdfd7781e6fec352fb49087138b61254ed26f5b4
beulahpriscilla <beulahpriscilla26@gmail.com>
2025-07-21 14:59:55 -0400

    Created create_task in db

--- app/crud.py
+++ app/crud.py
@@ -4,5 +4,10 @@ from . import models, schemas
 def get_tasks(db: Session):
   return db.query(models.Task).all()
 
-
+def create_task(db: Session, task: schemas.Task):
+  db_task = models.Task(title=task.title, completed=task.completed)
+  db.add(db_task)
+  db.commit()
+  db.refresh(db_task)
+  return db_task
 
