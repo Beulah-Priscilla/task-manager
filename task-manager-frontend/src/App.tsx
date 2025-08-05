c4de4ff25570dc122de6656ab85abbee561c1a1e
beulahpriscilla <beulahpriscilla26@gmail.com>
2025-08-05 14:49:33 -0400

    Created states

--- task-manager-frontend/src/App.tsx
+++ task-manager-frontend/src/App.tsx
@@ -1,1 +1,17 @@
 import axios from 'axios';
+
+import { Task } from './types'
+import React, { useState } from 'react';
+import { Container } from '@mui/material';
+
+const App: React.FC = () => {
+  const[tasks, setTasks] = useState<Task[]>([]);
+  const[loading, setLoading] = useState<boolean>(true);
+  return (
+<Container>
+  
+</Container>
+  );
+};
+
+export default App;
