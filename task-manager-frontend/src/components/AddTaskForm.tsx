6704b73f4feb0447e900e2cc93c2ca358d593c99
beulahpriscilla <beulahpriscilla26@gmail.com>
2025-08-06 10:35:48 -0400

    Added API for adding task in frontend

--- task-manager-frontend/src/components/AddTaskForm.tsx
+++ task-manager-frontend/src/components/AddTaskForm.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Task } from '../types';
-import { Box } from '@mui/material';
+import { Box, TextField } from '@mui/material';
 
 interface AddTaskFormProps {
   onTaskAdded: (task: Task) => void;
 }
 
 const AddTaskForm: React.FC<AddTaskFormProps> = ({ onTaskAdded }) => {
+  const [newTask, setNewTask] = useState<string>("");
+
+  const handleAddTask = () => {
+    if (!newTask.trim()) return;
+
+    axios
+      .post<Task>('http://localhost:8000/tasks', {
+        title: newTask,
+        completed: false,
+      })
+      .then((response) => {
+        onTaskAdded(response.data);
+        setNewTask("");
+      })
+      .catch((error) => console.log('Error adding task', error));
+  };
+
   return (
     <Box>
-
+      <TextField
+        label="New Task"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+      />
     </Box>
   )
 }
