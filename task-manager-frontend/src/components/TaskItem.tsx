158729f57d849c62bfd10e11e02a2dd65e9dafc6
beulahpriscilla <beulahpriscilla26@gmail.com>
2025-08-08 09:22:01 -0400

    Created the outline of frontend of Deleting Task

--- task-manager-frontend/src/components/TaskItem.tsx
+++ task-manager-frontend/src/components/TaskItem.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import axios from 'axios';
-import { Card } from '@mui/material';
+import { Card, CardContent, Checkbox, IconButton, Typography } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete'
+import { Task } from '../types';
 
-const TaskItem: React.FC = () => {
+interface TaskItemProps {
+  task: Task;
+  onTaskDeleted: (id: number) => void;
+}
+
+const TaskItem: React.FC<TaskItemProps> = ({ task, onTaskDeleted }) => {
   return(
     <Card>
-
+      <CardContent>
+        <Checkbox></Checkbox>
+        <Typography></Typography>
+        <IconButton>
+          <DeleteIcon />
+        </IconButton>
+      </CardContent>
     </Card>
   )
 }
